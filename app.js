6d5e427eeacb0cf263e6e933cfcb6e042e1e53f0
tenaz21 <tenazoa72922787@gmail.com>
2026-06-16 04:22:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,8 @@ const btn = document.getElementById("connectBtn");
 
 btn.onclick = () => {
 
-    const host = document.getElementById("host").value;
-    const user = document.getElementById("user").value;
-    const pass = document.getElementById("pass").value;
+    document.getElementById("loginPage").style.display = "none";
 
-    if(!host || !user || !pass){
+    document.getElementById("homePage").style.display = "flex";
 
-        document.getElementById("status").innerHTML =
-        "⚠️ Completa todos los campos.";
-
-        return;
-    }
-
-    document.getElementById("status").innerHTML =
-    "✅ Datos introducidos correctamente.";
 };
